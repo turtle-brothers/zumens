0f4bd9c978708d7f34cef1240dc66bfcd18784db
root <root@DESKTOP-D6QO17N.localdomain>
2023-07-08 19:38:26 +0900

    [Update] router and header update, footer creation
    component partitioning of routers and headers, footers.

---
+++ frontend/app/src/components/templates/FooterLayout.tsx
@@ -0,0 +1,17 @@
+import { memo, ReactNode, FC } from "react";
+
+import { Footer } from "../organisms/layout/Footer";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const FooterLayout: FC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      {children}
+      <Footer />
+    </>
+  );
+});
---
+++ frontend/app/src/router/Router.tsx
@@ -0,0 +1,84 @@
+import { memo, FC } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "../components/pages/Home";
+import { Login } from "../components/pages/Login";
+import { UserManagement } from "../components/pages/UserManagement";
+import { Upload } from "../components/pages/Upload";
+import { PrivacyPolicy } from "../components/pages/PrivacyPolicy";
+import { TermsOfUse } from "../components/pages/TermsOfUse";
+import { Page404 } from "../components/pages/Page404";
+
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { FooterLayout } from "../components/templates/FooterLayout";
+// import { LoginUserProvider } from "../providers/LoginUserProvider";
+
+export const Router: FC = memo(() => {
+  return (
+    <Routes>
+      {/* <LoginUserProvider> */}
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route
+          path="/home"
+          element={
+            <HeaderLayout>
+              <FooterLayout>
+                 <Home />
+              </FooterLayout>
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="/home/user_management"
+          element={
+            <HeaderLayout>
+              <FooterLayout>
+                <UserManagement />
+              </FooterLayout>
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="/home/upload"
+          element={
+            <HeaderLayout>
+              <FooterLayout>
+                <Upload />
+              </FooterLayout>
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="/home/privacy_policy"
+          element={
+            <HeaderLayout>
+              <FooterLayout>
+                <PrivacyPolicy />
+              </FooterLayout>
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="/home/terms_of_use"
+          element={
+            <HeaderLayout>
+              <FooterLayout>
+                <TermsOfUse />
+              </FooterLayout>
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <HeaderLayout>
+              <FooterLayout>
+              <Page404 />
+              </FooterLayout>
+            </HeaderLayout>
+          }
+        />
+      {/* </LoginUserProvider> */}
+    </Routes>
+  );
+});
