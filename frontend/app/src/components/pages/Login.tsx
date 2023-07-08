e02591484637267444b68757b666329467c07e82
root <root@DESKTOP-D6QO17N.localdomain>
2023-07-09 01:56:12 +0900

    [Update] Directory Update
    Update directories not maintained in github.

--- frontend/app/src/components/pages/Login.tsx
+++ frontend/app/src/components/pages/Login.tsx
@@ -1,5 +1,45 @@
-import { memo, FC } from "react";
+import { memo, FC, useState, ChangeEvent } from "react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: FC = memo(() => {
-  return <p>ログインページです</p>;
+  const { login, loading } = useAuth();
+  const [userId, setuserId] = useState("");
+  const onChangeUserId = (event: ChangeEvent<HTMLInputElement>) =>
+    setuserId(event.target.value);
+
+  const onClickLogin = () => login(userId);
+
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          図面管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={3} py={4} px={10}>
+          <Input
+            placeholder="ユーザーID"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          {/* パスワードのロジックを追加する */}
+          {/* <Input
+            placeholder="パスワード"
+            value={password}
+            onChange={onChangePassword}
+          /> */}
+          <PrimaryButton
+            disabled={userId === ""}
+            loading={loading}
+            onClick={onClickLogin}
+          >
+            ログイン
+          </PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
