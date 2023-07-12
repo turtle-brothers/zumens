ec4857fe6b18c9ae2372f724b7ec39150ca337c1
root <root@DESKTOP-D6QO17N.localdomain>
2023-07-12 11:23:46 +0900

    [Update] fixed menu bar
    split menu bar elements into components.

---
+++ frontend/app/src/components/atoms/menu/Menumenu.tsx
@@ -0,0 +1,23 @@
+import { Menu, MenuButton, Button, MenuList, MenuItem, BoxProps } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { FC } from "react";
+
+interface Props extends BoxProps {
+  menuTitle: string;
+  menuItems: string[];
+}
+
+export const Menumune: FC<Props> = ({ menuTitle, menuItems, ...props }) => {
+  return (
+    <Menu {...props}>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />} w="100%">
+        {menuTitle}
+      </MenuButton>
+      <MenuList>
+        {menuItems.map((item, index) => (
+          <MenuItem key={index}>{item}</MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
--- frontend/app/src/components/molecules/MenuSearch.tsx
+++ frontend/app/src/components/molecules/MenuSearch.tsx
@@ -1,132 +1,50 @@
 import { memo, FC } from "react";
-import { Flex, Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Flex, Box, Heading } from "@chakra-ui/react";
+import { EmailIcon, InfoIcon, PhoneIcon } from "@chakra-ui/icons";
+
+import { Menumune } from '../atoms/menu/Menumenu';
+import { MenuInput } from '../atoms/input/MenuInput';
 
 export const MenuSearch: FC = memo(() => {
+
   return (
     <>
-      {/* 1段目のメニューバー */}
-      <Flex
-        fontSize="sm"
-        flexGrow={2}
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
+      <Box border="2px" borderColor="blue.500" borderRadius="md" p={4} m={8} backgroundColor="gray.100">
+        <Heading as="h3" size="md" mb={4}>検索条件</Heading>
 
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
+        {/* 1段目のメニューバー */}
+        <Flex
+          fontSize="sm"
+          flexGrow={2}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <MenuInput type='mail' placeholder='e-mail' icon={<EmailIcon color='gray.300' />}/>
+          <MenuInput type='tel' placeholder='Phone number' icon={<PhoneIcon color='gray.300' />}/>
+          <MenuInput type='address' placeholder='Address' icon={<InfoIcon color='gray.300' />}/>
+          <MenuInput type='address' placeholder='Address'/>
+        </Flex>
 
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-      </Flex>
-
-      {/* 2段目のメニューバー */}
-      <Flex
-        fontSize="sm"
-        flexGrow={2}
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width={{ base: "ms", md: "md" }}>
-          Actions
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-      </Flex>
-
+        {/* 2段目のメニューバー */}
+        <Flex>
+          <Menumune
+            menuTitle='Actions'
+            menuItems={['Download', 'Create a Copy', 'Mark as Draft', 'Delete', 'Attend a Workshop']}
+          />
+          <Menumune
+            menuTitle='Alphabet'
+            menuItems={['A', 'B', 'C', 'D', 'E']}
+          />
+          <Menumune
+            menuTitle='Number'
+            menuItems={['1', '2', '3', '4', '5']}
+          />
+          <Menumune
+            menuTitle='Number'
+            menuItems={['1', '2', '3', '4', '5']}
+          />
+        </Flex>
+      </Box>
     </>
   );
 });
